4cf20820df72ac97789a0cc353ba7bedf5bf28c3
lengwe <wl4817@ic.ac.uk>
2020-06-14 13:14:30 +0100

    notification fully works and minor change on style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,10 @@ export class MyApp {
       // this.pushSetUp();
 
       //using testingtask app
-      Parse.initialize("8LMcCxaA5uezEUsY0mefPXDaX1g3GFai0fZQbOlh", "wXeWeEjSPbiL06TA0G0WasnB9rVH4wxmiRjXiNbj");
+      Parse.initialize("xJ7F9Uixopap8zn7WGlXoWdQMtNaH3DmamCMnMI1", "TsOqkbfPfY7gUDoDluOJ59e5r3lBYLKLeBUSsmya");
       // Parse.initialize("XiiCvvh3t61q2vMSQornvYKPPsPcTszpsMB0tpbT", "UMEsWQpchNtjV5XCyaA6uXaZOAiXO5x73vObhzKv");
       Parse.serverURL = 'https://parseapi.back4app.com/';
-      Parse.liveQueryServerURL = 'ws://haha.back4app.io';
+      Parse.liveQueryServerURL = 'ws://testingtask.back4app.io';
 
       Parse.User.currentAsync().then(user => {
         console.log('Logged user', user);
@@ -38,34 +38,6 @@ export class MyApp {
       })
     });
   }
-
-  // pushSetUp(){
-  //   const options: PushOptions = {
-  //     android: {
-  //         iconColor:'red',
-  //         icon:'notify',
-  //         sound:true,
-  //         vibrate: true
-  //       }
-  //   };
-  //   const pushObject: PushObject = this.push.init(options);
-
-  //   this.push.createChannel({
-  //     id: "testchannel1",
-  //     description: "My first test channel",
-  //     // The importance property goes from 1 = Lowest, 2 = Low, 3 = Normal, 4 = High and 5 = Highest.
-  //     importance: 4
-  //    }).then(() => console.log('Channel created'));
-
-  //    pushObject.on('notification').subscribe((notification: any) => {
-  //     console.log(notification)
-  //     let youralert = this.alertCtrl.create({
-  //       title: 'Warning: Low Charging Level',
-  //       message: 'Please charge your car ASAP!'
-  //    });
-  //     youralert.present();
-  //   });
-  //   }
-  }
+}
 
 
